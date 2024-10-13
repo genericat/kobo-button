d457f1d0bd06111c6a42ddff2f1c74d73f4491b7
genericat <ssalwa606@gmail.com>
2024-10-14 01:38:39 +0700

    Update layout

--- src/script.js
+++ src/script.js
@@ -1,1 +1,31 @@
-console.log('Test');
+const infoBtn = document.getElementById('info-btn');
+
+infoBtn.onclick = () => {
+    const noticeEl = document.getElementById('notice-info');
+    const isExpanded = infoBtn.getAttribute('aria-expanded');
+
+    if (isExpanded === 'false') {
+        noticeEl.classList.remove('hidden');
+        infoBtn.setAttribute('aria-expanded', 'true');
+    } else {
+        noticeEl.classList.add('hidden');
+        infoBtn.setAttribute('aria-expanded', 'false');
+    }
+}
+
+document.onclick = (e) => {
+    const isExpanded = infoBtn.getAttribute('aria-expanded');
+
+    if (isExpanded === 'false') {
+        return;
+    }
+
+    const noticeEl = document.getElementById('notice-info');
+
+    if (noticeEl.contains(e.target) || infoBtn.contains(e.target)) {
+        return;
+    }
+
+    noticeEl.classList.add('hidden');
+    infoBtn.setAttribute('aria-expanded', 'false');
+}
