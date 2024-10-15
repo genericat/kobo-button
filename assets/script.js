cebd4bb748ac6283709f4f45dd1dfc3c643daf07
genericat <ssalwa606@gmail.com>
2024-10-16 02:12:20 +0700

    Update playlist window

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,46 @@
 const infoBtn = document.getElementById('info-btn');
 
 infoBtn.onclick = () => {
-    const noticeEl = document.getElementById('notice-info');
-    const isExpanded = infoBtn.getAttribute('aria-expanded');
+  const noticeEl = document.getElementById('notice-info');
+  const isExpanded = infoBtn.getAttribute('aria-expanded');
 
-    if (isExpanded === 'false') {
-        noticeEl.classList.remove('hidden');
-        infoBtn.setAttribute('aria-expanded', 'true');
-    } else {
-        noticeEl.classList.add('hidden');
-        infoBtn.setAttribute('aria-expanded', 'false');
-    }
+  if (isExpanded === 'false') {
+    noticeEl.classList.remove('hidden');
+    infoBtn.setAttribute('aria-expanded', 'true');
+  } else {
+    noticeEl.classList.add('hidden');
+    infoBtn.setAttribute('aria-expanded', 'false');
+  }
+}
+
+const playlistBtn = document.getElementById('playlist-btn');
+
+playlistBtn.onclick = () => {
+  const playlistEl = document.getElementById('playlist-window');
+  const isExpanded = playlistBtn.getAttribute('aria-expanded');
+
+  if (isExpanded === 'false') {
+    playlistEl.classList.remove('hidden');
+    playlistBtn.setAttribute('aria-expanded', 'true');
+  } else {
+    playlistEl.classList.add('hidden');
+    playlistBtn.setAttribute('aria-expanded', 'false');
+  }
 }
 
 document.onclick = (e) => {
-    const isExpanded = infoBtn.getAttribute('aria-expanded');
+  const isExpanded = infoBtn.getAttribute('aria-expanded');
 
-    if (isExpanded === 'false') {
-        return;
-    }
+  if (isExpanded === 'false') {
+    return;
+  }
 
-    const noticeEl = document.getElementById('notice-info');
+  const noticeEl = document.getElementById('notice-info');
 
-    if (noticeEl.contains(e.target) || infoBtn.contains(e.target)) {
-        return;
-    }
+  if (noticeEl.contains(e.target) || infoBtn.contains(e.target)) {
+    return;
+  }
 
-    noticeEl.classList.add('hidden');
-    infoBtn.setAttribute('aria-expanded', 'false');
+  noticeEl.classList.add('hidden');
+  infoBtn.setAttribute('aria-expanded', 'false');
 }
