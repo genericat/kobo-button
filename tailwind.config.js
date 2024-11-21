cdeeabe7aeac8ffb5f84a264a07b1d2434c9f15f
genericat <ssalwa606@gmail.com>
2024-11-22 01:06:38 +0700

    Color management

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,11 @@ module.exports = {
         'pattern': "url('/assets/img/pattern.svg')"
       },
       colors: {
+        'dark-blue-p3': '#093CB7',
+        'light-blue-p3': '#65D5F4',
         'dark-blue': '#07377E',
         'light-blue': '#9CE7f8',
-        'accent': '#EADBA8'
+        'accent': '#EADBA8',
       }
     },
   },
